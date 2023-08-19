4e4ac48ad1ca60b9a87e3fabd80a562bf59330d1
Fatima Haouari <71061623+Fatima-Haouari@users.noreply.github.com>
2023-08-19 20:27:14 +0300

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -8,7 +8,6 @@ import json
 import codecs
 import gzip
 import os
-from googletrans import Translator
 import pandas as pd
 import glob
 from json import loads
@@ -18,20 +17,9 @@ import re
 import numpy as np
 
 ######################clean text######################################
-def clean(text):
-   print(text, type(text))
-   if text is np.nan:
-    return ""
-   else:
-    text = re.sub(r"http\S+", " ", text) # remove urls
-    text = re.sub(r"RT ", " ", text) # remove rt
-    text = re.sub(r"@[\w]*", " ", text) # remove handles
-    text = re.sub(r"[\.\,\#_\|\:\?\?\/\=]", " ", text) # remove special characters
-    text = re.sub(r'\t', ' ', text) # remove tabs
-    text = re.sub(r'\n', ' ', text) # remove line jump
-    text = re.sub(r"\s+", " ", text) # remove extra white space
+def clean_text(text):
     accents = re.compile(r'[\u064b-\u0652\u0640]') # harakaat and tatweel (kashida) to remove
-    arabic_punc= re.compile(r'[\u0621-\u063A\u0641-\u064A\d+]+') # Keep only Arabic letters/do not remove numbers
+    arabic_punc= re.compile(r'[\u0621-\u063A\u0641-\u064A\d+]+') # Keep only Arabic letters/do not remove number
     text=' '.join(arabic_punc.findall(accents.sub('',text)))
     text = text.strip()
     return text
@@ -48,16 +36,5 @@ def normalize(text):
     text = re.sub("ة", "ه", text)
     return(text)
 #######################################################################
-def translate(text):
-    #source#https://github.com/ssut/py-googletrans/issues/268
-    url = "https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=ar&q=" + text
-    headers = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'
-    }
 
-    try:
-      request_result = requests.get(url, headers=headers).json()
-    except:
-     pass
-    return request_result['alternative_translations'][0]['alternative'][0]['word_postproc']
 

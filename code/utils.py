68cb69fba5d7b1bf307a07bc5db0f05e68bca081
Fatima Haouari <71061623+Fatima-Haouari@users.noreply.github.com>
2023-08-19 20:31:18 +0300

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -16,10 +16,29 @@ import requests
 import re
 import numpy as np
 
+######################clean tweets######################################
+def clean_tweets(text):
+   if text is np.nan:
+    return ""
+   else:
+    text = re.sub(r"http\S+", " ", text) # remove urls
+    text = re.sub(r"RT ", " ", text) # remove rt
+    text = re.sub(r"@[\w]*", " ", text) # remove handles
+    text = re.sub(r"[\.\,\#_\|\:\?\?\/\=]", " ", text) # remove special characters
+    text = re.sub(r'\t', ' ', text) # remove tabs
+    text = re.sub(r'\n', ' ', text) # remove line jump
+    text = re.sub(r"\s+", " ", text) # remove extra white space
+    accents = re.compile(r'[\u064b-\u0652\u0640]') # harakaat and tatweel (kashida) to remove
+     
+    arabic_punc= re.compile(r'[\u0621-\u063A\u0641-\u064A\d+]+') # Keep only Arabic letters/do not remove numbers
+    text=' '.join(arabic_punc.findall(accents.sub('',text)))
+    text = text.strip()
+    return text
+  
 ######################clean text######################################
 def clean_text(text):
     accents = re.compile(r'[\u064b-\u0652\u0640]') # harakaat and tatweel (kashida) to remove
-    arabic_punc= re.compile(r'[\u0621-\u063A\u0641-\u064A\d+]+') # Keep only Arabic letters/do not remove number
+    arabic_punc= re.compile(r'[\u0621-\u063A\u0641-\u064A\d+]+') # Keep only Arabic letters/do not remove numbers
     text=' '.join(arabic_punc.findall(accents.sub('',text)))
     text = text.strip()
     return text
